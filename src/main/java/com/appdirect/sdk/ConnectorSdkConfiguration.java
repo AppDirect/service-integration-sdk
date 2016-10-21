4f7d64a43ade3317729d4f378456055a78798752
EmilDafinov <emil.dafinov@appdirect.com>
2016-10-21 14:56:24 -0400

    Added root configuration that clients should consume.

---
+++ src/main/java/com/appdirect/sdk/ConnectorSdkConfiguration.java
@@ -0,0 +1,10 @@
+package com.appdirect.sdk;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.appdirect.sdk")
+public class ConnectorSdkConfiguration {
+
+}
