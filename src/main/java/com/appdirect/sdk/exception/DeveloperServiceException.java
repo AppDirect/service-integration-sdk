5b6c8fe74d7413b0de77262666d90711ec5bfa55
Gabriel Rubenovitch <gabriel.rubenovitch@appdirect.com>
2016-10-31 15:29:06 -0400

    Getter is closer to point of use.

--- src/main/java/com/appdirect/sdk/exception/DeveloperServiceException.java
+++ src/main/java/com/appdirect/sdk/exception/DeveloperServiceException.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 import com.appdirect.sdk.appmarket.api.APIResult;
 import com.appdirect.sdk.appmarket.api.ErrorCode;
 
-@Getter
 public class DeveloperServiceException extends RuntimeException {
 	private static final long serialVersionUID = 6079855456255852065L;
 
+	@Getter
 	private final APIResult result;
 
 	public DeveloperServiceException(String message) {
