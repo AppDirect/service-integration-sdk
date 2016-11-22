c2fc41ca7d549ecb54be9dc1b472c7d865e7a0cb
EmilDafinov <emil.dafinov@appdirect.com>
2016-11-22 09:47:22 -0500

    Javadoc for the events

--- src/main/java/com/appdirect/sdk/appmarket/api/SubscriptionClosed.java
+++ src/main/java/com/appdirect/sdk/appmarket/api/SubscriptionClosed.java
@@ -1,5 +1,11 @@
 package com.appdirect.sdk.appmarket.api;
 
+/**
+ * Represents one of the subtypes of the Subscription Notice event sent by the App Market
+ * See the documentation at the link below for more detailed information regarding the significance of the event 
+ * @see <a href="https://docs.appdirect.com/developer/distribution/event-notifications/subscription-events#notice-types">SUBSCRIPTION_NOTICE types</a>
+ * 
+ */
 public class SubscriptionClosed {
 	private final AccountInfo accountInfo;
 
