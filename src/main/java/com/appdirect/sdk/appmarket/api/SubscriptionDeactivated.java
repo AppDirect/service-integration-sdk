938195c3ce39a1a8bab04218ea365a8a739fb1e5
EmilDafinov <emil.dafinov@appdirect.com>
2016-11-22 10:11:53 -0500

    More javadoc

--- src/main/java/com/appdirect/sdk/appmarket/api/SubscriptionDeactivated.java
+++ src/main/java/com/appdirect/sdk/appmarket/api/SubscriptionDeactivated.java
@@ -1,8 +1,11 @@
 package com.appdirect.sdk.appmarket.api;
 
 /**
- * Represents one of the subtypes of the Subscription Notice event sent by the App Market
- * See the documentation at the link below for more detailed information regarding the significance of the event 
+ * Represents one of the subtypes of the Subscription Notice event sent by the App Market.
+ * See the documentation at the link below for more detailed information regarding the significance of the event.
+ * 
+ * Note that when {@link SubscriptionDeactivated} is handled in a connector, the data for the deactivated account
+ * SHOULD NOT be deleted, because it can be reactivated later (see {@link SubscriptionReactivated}).
  * @see <a href="https://docs.appdirect.com/developer/distribution/event-notifications/subscription-events#notice-types">SUBSCRIPTION_NOTICE types</a>
  *
  */
