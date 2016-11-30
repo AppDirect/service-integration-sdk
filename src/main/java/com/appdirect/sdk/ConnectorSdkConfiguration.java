a9f18b6de15c0ac13376c95772c2e7c1bd233f91
EmilDafinov <emil.dafinov@appdirect.com>
2016-11-30 10:25:58 -0500

    removed the email service from the SDK-provided Spring configuration

--- src/main/java/com/appdirect/sdk/ConnectorSdkConfiguration.java
+++ src/main/java/com/appdirect/sdk/ConnectorSdkConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Import;
 import com.appdirect.sdk.appmarket.events.AppmarketCommunicationConfiguration;
 import com.appdirect.sdk.appmarket.events.DeveloperExceptionHandler;
 import com.appdirect.sdk.appmarket.events.EventHandlingConfiguration;
-import com.appdirect.sdk.notification.NotificationConfiguration;
 import com.appdirect.sdk.web.RestOperationsFactory;
 import com.appdirect.sdk.web.config.JacksonConfiguration;
 import com.appdirect.sdk.web.oauth.SecurityConfiguration;
@@ -15,11 +14,10 @@ import com.appdirect.sdk.web.exception.AppmarketEventConsumerExceptionHandler;
 
 @Configuration
 @Import({
-	JacksonConfiguration.class, 
-	SecurityConfiguration.class, 
-	EventHandlingConfiguration.class, 
-	AppmarketCommunicationConfiguration.class,
-	NotificationConfiguration.class
+	JacksonConfiguration.class,
+	SecurityConfiguration.class,
+	EventHandlingConfiguration.class,
+	AppmarketCommunicationConfiguration.class 
 })
 public class ConnectorSdkConfiguration {
 
