82146af6b15e255bcb7818afdb9676d5b8fe86bc
Gabriel Rubenovitch <gabriel.rubenovitch@appdirect.com>
2016-11-30 14:42:36 -0500

    PI-6624: minor serialVersionUID warning fix.

--- src/main/java/com/appdirect/sdk/notification/SendNotificationFailedException.java
+++ src/main/java/com/appdirect/sdk/notification/SendNotificationFailedException.java
@@ -1,6 +1,8 @@
 package com.appdirect.sdk.notification;
 
 public class SendNotificationFailedException extends Exception {
+	private static final long serialVersionUID = -3487516993124221348L;
+
 	SendNotificationFailedException(String message, Throwable cause) {
 		super(message, cause);
 	}
