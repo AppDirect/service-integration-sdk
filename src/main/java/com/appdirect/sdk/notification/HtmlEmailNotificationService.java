fc5d2283c7145681b0ac0862b821ea45b3a67773
EmilDafinov <emil.dafinov@appdirect.com>
2016-11-30 10:25:30 -0500

    Tests & comments

--- src/main/java/com/appdirect/sdk/notification/HtmlEmailNotificationService.java
+++ src/main/java/com/appdirect/sdk/notification/HtmlEmailNotificationService.java
@@ -12,14 +12,14 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 @Slf4j
 @RequiredArgsConstructor
 public class HtmlEmailNotificationService {
-	private static final String FROM_ADDRESS = "notifications@appdirect.com";
+	private final String fromAddress;
 	private final JavaMailSender sender;
 
 	public void sendHtmlEmail(String messageSubject, String messageBody, String toAddress) throws SendNotificationFailedException {
 
 		try {
 			MimeMessageHelper helper = new MimeMessageHelper(sender.createMimeMessage());
-			helper.setFrom(new InternetAddress(FROM_ADDRESS));
+			helper.setFrom(new InternetAddress(fromAddress));
 			helper.setTo(new InternetAddress(toAddress));
 			helper.setSubject(messageSubject);
 			helper.setText(messageBody, true);
@@ -27,7 +27,7 @@ public class HtmlEmailNotificationService {
 		} catch (MessagingException e) {
 			String errorMessage = String.format(
 				"Failed sending email notification with from=%s, to=%s, notification=%s",
-				FROM_ADDRESS,
+				fromAddress,
 				toAddress,
 				messageBody
 			);
