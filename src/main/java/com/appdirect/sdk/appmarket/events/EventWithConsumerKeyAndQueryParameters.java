ace7d17f4cc1e7844c8e81a4ab9c99c666791bfa
Gabriel Rubenovitch <gabriel.rubenovitch@appdirect.com>
2016-12-12 12:17:15 -0500

    PI-6967: added some javadoc to getter fields.

--- src/main/java/com/appdirect/sdk/appmarket/events/EventWithConsumerKeyAndQueryParameters.java
+++ src/main/java/com/appdirect/sdk/appmarket/events/EventWithConsumerKeyAndQueryParameters.java
@@ -17,9 +17,20 @@ import lombok.ToString;
 @EqualsAndHashCode
 @RequiredArgsConstructor
 public abstract class EventWithConsumerKeyAndQueryParameters {
+	/**
+	 * Returns the consumer key that was used by the appmarket to publish this event.
+	 * You can use this to determine which product is the originator of this event.
+	 */
 	private final String consumerKeyUsedByTheRequest;
 	private final Map<String, String[]> queryParameters;
 
+	/**
+	 * Returns the query parameters that were passed to the endpoint when this event was received.
+	 * i.e. calling <code>/processEvent?eventUrl=some-url&themeColor=yellow&themeColor=red</code> would yield
+	 * a map with 2 entries: <code>eventUrl=[some-url]</code> and <code>themeColor=[yellow, red]</code>.
+	 *
+	 * @return the query parameters map.
+	 */
 	public Map<String, String[]> getQueryParameters() {
 		return new HashMap<>(queryParameters);
 	}
