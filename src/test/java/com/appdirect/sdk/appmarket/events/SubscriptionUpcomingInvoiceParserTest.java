7bb226f3a3b70bdc373d51a6aa505b8879bf7d04
Gabriel Rubenovitch <gabriel.rubenovitch@appdirect.com>
2016-12-12 15:00:47 -0500

    PI-6967: Added consumer key & query params to the SubscriptionUpcomingInvoice event.

---
+++ src/test/java/com/appdirect/sdk/appmarket/events/SubscriptionUpcomingInvoiceParserTest.java
@@ -0,0 +1,53 @@
+package com.appdirect.sdk.appmarket.events;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+import static org.assertj.core.util.Arrays.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class SubscriptionUpcomingInvoiceParserTest {
+	private SubscriptionUpcomingInvoiceParser parser = new SubscriptionUpcomingInvoiceParser();
+
+	@Test
+	public void parse_extractsTheConsumerKey() throws Exception {
+		SubscriptionUpcomingInvoice parsedEvent = parser.parse("the-key", someEvent(), someQueryParams());
+
+		assertThat(parsedEvent.getConsumerKeyUsedByTheRequest()).isEqualTo("the-key");
+	}
+
+	@Test
+	public void parse_extractsTheAccountInfo() throws Exception {
+		SubscriptionUpcomingInvoice parsedEvent = parser.parse("the-key", someEventFor("big-account"), someQueryParams());
+
+		assertThat(parsedEvent.getAccountInfo().getAccountIdentifier()).isEqualTo("big-account");
+	}
+
+	@Test
+	public void parse_extractsTheQueryParams() throws Exception {
+		SubscriptionUpcomingInvoice parsedEvent = parser.parse("the-key", someEvent(), someQueryParams("hello", "the"));
+
+		assertThat(parsedEvent.getQueryParameters()).containsOnly(entry("hello", array("the")));
+	}
+
+	private EventInfo someEvent() {
+		return someEventFor("some-account");
+	}
+
+	private EventInfo someEventFor(String accountId) {
+		return EventInfo.builder().payload(EventPayload.builder().account(AccountInfo.builder().accountIdentifier(accountId).build()).build()).build();
+	}
+
+	private Map<String, String[]> someQueryParams() {
+		return someQueryParams("param1", "value1");
+	}
+
+	private Map<String, String[]> someQueryParams(String key, String... values) {
+		Map<String, String[]> queryParams = new HashMap<>();
+		queryParams.put(key, values);
+		return queryParams;
+	}
+}
