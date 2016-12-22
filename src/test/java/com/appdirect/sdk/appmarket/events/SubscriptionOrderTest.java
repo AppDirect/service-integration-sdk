4cd2859f22c8ca4e4588372445b4ff0ae43c86e9
Gabriel Rubenovitch <gabriel.rubenovitch@appdirect.com>
2016-12-22 15:07:01 -0500

    PI-6525: Added test

---
+++ src/test/java/com/appdirect/sdk/appmarket/events/SubscriptionOrderTest.java
@@ -0,0 +1,25 @@
+package com.appdirect.sdk.appmarket.events;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+public class SubscriptionOrderTest {
+	@Test
+	public void getQueryParameters_onEvent_neverReturnsNull() throws Exception {
+		SubscriptionOrder event = new SubscriptionOrder(
+				"key",
+				null,
+				UserInfo.builder().build(),
+				new HashMap<>(),
+				CompanyInfo.builder().build(),
+				OrderInfo.builder().build(),
+				"partner",
+				"appUuid",
+				null);
+
+		assertThat(event.getQueryParameters()).isNotNull().isEmpty();
+	}
+}
