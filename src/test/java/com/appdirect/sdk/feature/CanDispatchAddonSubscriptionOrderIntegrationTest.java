880477aca2cb2b9304579ba42b8838e1e9b7add2
Gabriel Rubenovitch <gabriel.rubenovitch@appdirect.com>
2017-01-04 11:59:22 -0500

    PI-6525: feature test fixes following the rebase with emil's changes.

--- src/test/java/com/appdirect/sdk/feature/CanDispatchAddonSubscriptionOrderIntegrationTest.java
+++ src/test/java/com/appdirect/sdk/feature/CanDispatchAddonSubscriptionOrderIntegrationTest.java
@@ -34,15 +34,15 @@ public class CanDispatchAddonSubscriptionOrderIntegrationTest {
 
 	@Test
 	public void addonSubscriptionOrderIsProcessedSuccessfully() throws Exception {
-		HttpResponse response = fakeAppmarket.sendEventTo(connectorEventEndpoint(), "/v1/events/order-addon");
+		HttpResponse response = fakeAppmarket.sendEventTo(connectorEventEndpoint(), "/v1/events/subscription-order-addon");
 
-		assertThat(fakeAppmarket.allRequestPaths()).first().isEqualTo("/v1/events/order-addon");
+		assertThat(fakeAppmarket.allRequestPaths()).first().isEqualTo("/v1/events/subscription-order-addon");
 		assertThat(response.getStatusLine().getStatusCode()).isEqualTo(202);
-		assertThat(EntityUtils.toString(response.getEntity())).isEqualTo("{\"success\":true,\"message\":\"Event with eventId=order-addon has been accepted by the connector. It will be processed soon.\"}");
+		assertThat(EntityUtils.toString(response.getEntity())).isEqualTo("{\"success\":true,\"message\":\"Event with eventId=subscription-order-addon has been accepted by the connector. It will be processed soon.\"}");
 
 		fakeAppmarket.waitForResolvedEvents(1);
-		assertThat(fakeAppmarket.resolvedEvents()).contains("order-addon");
-		assertThat(fakeAppmarket.allRequestPaths()).last().isEqualTo("/api/integration/v1/events/order-addon/result");
+		assertThat(fakeAppmarket.resolvedEvents()).contains("subscription-order-addon");
+		assertThat(fakeAppmarket.allRequestPaths()).last().isEqualTo("/api/integration/v1/events/subscription-order-addon/result");
 		assertThat(fakeAppmarket.lastRequestBody()).isEqualTo("{\"success\":true,\"message\":\"ADDON_ORDER has been processed just now.\"}");
 	}
 
