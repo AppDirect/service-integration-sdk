b9c92e5c9a74d04ad0df6fd73a522b01151e3ae9
EmilDafinov <EmilDafinov@users.noreply.github.com>
2017-01-04 11:12:28 -0500

    Feature/random ports (#42)
    * Created a method returning a free port and redid the SMTP test so that it uses it
    
    * added a link to the source
    
    * Sonar warnings
    
    * Moved util method to the test sources
    
    * the forgotten delete:)

---
+++ src/test/java/com/appdirect/sdk/utils/PortUtils.java
@@ -0,0 +1,25 @@
+package com.appdirect.sdk.utils;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public class PortUtils { //NOSONAR, the private constructor is created by Lombok
+
+	/**
+	 * @return a random, unused TCP port
+	 * @see <a href="https://gist.github.com/vorburger/3429822">Source gist</a>
+	 */
+	public static int getRandomFreePort() {
+		try (ServerSocket socket = new ServerSocket(0)) {
+			socket.setReuseAddress(true);
+			return socket.getLocalPort();
+		} catch (IOException e) {
+			throw new RuntimeException("Failed getting a free port", e);
+		}
+	}
+}
