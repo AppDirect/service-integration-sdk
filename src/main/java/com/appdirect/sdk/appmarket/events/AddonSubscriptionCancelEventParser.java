9c3920daf6ab204b35e08e4c490485af43c76238
EmilDafinov <emil.dafinov@appdirect.com>
2017-01-05 12:17:52 -0500

    cancel event

---
+++ src/main/java/com/appdirect/sdk/appmarket/events/AddonSubscriptionCancelEventParser.java
@@ -0,0 +1,14 @@
+package com.appdirect.sdk.appmarket.events;
+
+class AddonSubscriptionCancelEventParser implements EventParser<AddonSubscriptionCancel> {
+	@Override
+	public AddonSubscriptionCancel parse(EventInfo eventInfo, EventHandlingContext eventContext) {
+		return new AddonSubscriptionCancel(
+			eventInfo.getPayload().getAccount().getAccountIdentifier(),
+			eventInfo.getPayload().getAccount().getParentAccountIdentifier(),
+			eventContext.getConsumerKeyUsedByTheRequest(),
+			eventContext.getQueryParameters(),
+			eventInfo.getFlag()
+		);
+	}
+}
