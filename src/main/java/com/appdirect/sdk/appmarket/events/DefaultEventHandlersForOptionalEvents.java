909c61e68b99950bf073d540c06190cbd36c60b7
Gabriel Rubenovitch <gabriel.rubenovitch@appdirect.com>
2017-01-17 09:25:47 -0500

    Sonar fix

--- src/main/java/com/appdirect/sdk/appmarket/events/DefaultEventHandlersForOptionalEvents.java
+++ src/main/java/com/appdirect/sdk/appmarket/events/DefaultEventHandlersForOptionalEvents.java
@@ -13,50 +13,50 @@ import com.appdirect.sdk.appmarket.AppmarketEventHandler;
  * and Spring will pick them up instead of defaulting to the "dummy" ones.
  */
 public class DefaultEventHandlersForOptionalEvents {
-	private final static String DEFAULT_SUCCESS = "This connector does nothing with this event.";
+	private static final String DEFAULT_SUCCESS = "This connector does nothing with this event.";
 
 	@Bean
 	public AppmarketEventHandler<SubscriptionChange> subscriptionChangeHandler() {
-		return (e) -> success(DEFAULT_SUCCESS);
+		return e -> success(DEFAULT_SUCCESS);
 	}
 
 	@Bean
 	public AppmarketEventHandler<SubscriptionClosed> subscriptionClosedHandler() {
-		return (e) -> success(DEFAULT_SUCCESS);
+		return e -> success(DEFAULT_SUCCESS);
 	}
 
 	@Bean
 	public AppmarketEventHandler<SubscriptionDeactivated> subscriptionDeactivatedHandler() {
-		return (e) -> success(DEFAULT_SUCCESS);
+		return e -> success(DEFAULT_SUCCESS);
 	}
 
 	@Bean
 	public AppmarketEventHandler<SubscriptionReactivated> subscriptionReactivatedHandler() {
-		return (e) -> success(DEFAULT_SUCCESS);
+		return e -> success(DEFAULT_SUCCESS);
 	}
 
 	@Bean
 	public AppmarketEventHandler<SubscriptionUpcomingInvoice> subscriptionUpcomingInvoiceHandler() {
-		return (e) -> success(DEFAULT_SUCCESS);
+		return e -> success(DEFAULT_SUCCESS);
 	}
 
 	@Bean
 	public AppmarketEventHandler<AddonSubscriptionOrder> addonSubscriptionOrderHandler() {
-		return (e) -> success(DEFAULT_SUCCESS);
+		return e -> success(DEFAULT_SUCCESS);
 	}
 
 	@Bean
 	public AppmarketEventHandler<AddonSubscriptionCancel> addonSubscriptionCancelHandler() {
-		return (e) -> success(DEFAULT_SUCCESS);
+		return e -> success(DEFAULT_SUCCESS);
 	}
 
 	@Bean
 	public AppmarketEventHandler<UserAssignment> userAssignmentHandler() {
-		return (e) -> success(DEFAULT_SUCCESS);
+		return e -> success(DEFAULT_SUCCESS);
 	}
 
 	@Bean
 	public AppmarketEventHandler<UserUnassignment> userUnassignmentHandler() {
-		return (e) -> success(DEFAULT_SUCCESS);
+		return e -> success(DEFAULT_SUCCESS);
 	}
 }
