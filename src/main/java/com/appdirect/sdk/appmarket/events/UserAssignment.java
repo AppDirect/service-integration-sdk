f3816ddecd806ada6ff5a6e4b7dc4ad33c859a90
Ian Beckles <ian.beckles@appdirect.com>
2017-05-03 10:18:26 -0700

    Renaming to assignedUser.

--- src/main/java/com/appdirect/sdk/appmarket/events/UserAssignment.java
+++ src/main/java/com/appdirect/sdk/appmarket/events/UserAssignment.java
@@ -8,12 +8,12 @@ import lombok.Getter;
 @Getter
 @EqualsAndHashCode(callSuper = true)
 public class UserAssignment extends EventWithConsumerKeyQueryParametersAndEventFlag {
-	private final UserInfo userInfo;
+	private final UserInfo assignedUser;
 	private final String accountId;
 
-	public UserAssignment(UserInfo userInfo, String accountId, String consumerKey, Map<String, String []> queryParameters, EventFlag eventFlag) {
+	public UserAssignment(UserInfo assignedUser, String accountId, String consumerKey, Map<String, String []> queryParameters, EventFlag eventFlag) {
 		super(consumerKey, queryParameters, eventFlag);
-		this.userInfo = userInfo;
+		this.assignedUser = assignedUser;
 		this.accountId = accountId;
 	}
 }
