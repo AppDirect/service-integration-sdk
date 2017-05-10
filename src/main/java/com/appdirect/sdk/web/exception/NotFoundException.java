40dbe8897b5bfbcb512ba016a9da9659831c5f02
EmilDafinov <emil.dafinov@appdirect.com>
2017-05-10 15:41:05 -0400

    removed unused class

--- src/main/java/com/appdirect/sdk/web/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.appdirect.sdk.web.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class NotFoundException extends RuntimeException {
-}
