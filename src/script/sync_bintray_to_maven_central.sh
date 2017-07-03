627a540004491608b92dbebf7c51e25399abf740
EmilDafinov <emil.dafinov@appdirect.com>
2017-07-03 16:43:14 -0400

    Added a script for uploading binaries to Maven Central and ensured it is called after deploy

---
+++ src/script/sync_bintray_to_maven_central.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+export BINTRAY_REPOSITORY_NAME="maven"
+export BINTRAY_PACKAGE_NAME="service-integration-sdk"
+export BINTRAY_PACKAGE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v "^\[" | grep -v "Listening")
+
+curl -i \
+	--user $CI_DEPLOY_USERNAME:$CI_DEPLOY_PASSWORD \
+	--header "Accept: application/json" \
+	--header "Content-Type: application/json" \
+	--request POST \
+	--data '{"username": "'$SONAR_USER'", "password": "'$SONAR_USER_PASSWORD'", "close": "1"}' \
+	https://api.bintray.com/maven_central_sync/$CI_DEPLOY_USERNAME/$BINTRAY_REPOSITORY_NAME/$BINTRAY_PACKAGE_NAME/versions/$BINTRAY_PACKAGE_VERSION
