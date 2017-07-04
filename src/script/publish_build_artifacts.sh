8b5780b7bf9c0ffb749cee932b656fcaa368c2d5
EmilDafinov <emil.dafinov@appdirect.com>
2017-07-04 16:47:33 -0400

    fake pass and verbose output

--- src/script/publish_build_artifacts.sh
+++ src/script/publish_build_artifacts.sh
@@ -14,11 +14,11 @@ echo "Syncing project version ${BINTRAY_PACKAGE_VERSION} with maven central ..."
 export BINTRAY_REQUEST_URL="https://api.bintray.com/maven_central_sync/appdirect-opensource/${BINTRAY_REPOSITORY_NAME}/${BINTRAY_PACKAGE_NAME}/versions/${BINTRAY_PACKAGE_VERSION}"
 echo "Bintray request URL: [$BINTRAY_REQUEST_URL]"
 curl -i \
-	--user ${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD} \
+	--user fakeUser:fakePass \
 	--header "Accept: application/json" \
 	--header "Content-Type: application/json" \
 	--request POST \
 	--data '{"username": "'${SONAR_USER}'", "password": "'${SONAR_USER_PASSWORD}'", "close": "1"}' \
-	${BINTRAY_REQUEST_URL}
+	${BINTRAY_REQUEST_URL} -v
 
 
