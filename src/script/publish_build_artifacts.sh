ef69969b515c6381914987dc711ea73bceaf027a
EmilDafinov <emil.dafinov@appdirect.com>
2017-07-04 15:14:37 -0400

    Third attempt at fixing the deployment scripts

--- src/script/publish_build_artifacts.sh
+++ src/script/publish_build_artifacts.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
+cd $TRAVIS_BUILD_DIR 
+
 #Note: All environment variables not defined in this build are assumed to be already present. Check your Travis build settings
 export BINTRAY_REPOSITORY_NAME="maven"
 export BINTRAY_PACKAGE_NAME="service-integration-sdk"
 export BINTRAY_PACKAGE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v "^\[" | grep -v "Listening")
 
-cd $TRAVIS_BUILD_DIR 
-
-#Publish artifacts to Bintray
+echo "Publishing build artifacts to Bintray..."
 mvn source:jar javadoc:jar deploy --settings settings.xml
 
-#Sync Bintray with Maven central
+echo "Syncing project versoion ${BINTRAY_PACKAGE_VERSION} with maven central ..."
+
 curl -i \
-	--user $CI_DEPLOY_USERNAME:$CI_DEPLOY_PASSWORD \
+	--user ${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD} \
 	--header "Accept: application/json" \
 	--header "Content-Type: application/json" \
 	--request POST \
-	--data '{"username": "'$SONAR_USER'", "password": "'$SONAR_USER_PASSWORD'", "close": "1"}' \
-	https://api.bintray.com/maven_central_sync/$CI_DEPLOY_USERNAME/$BINTRAY_REPOSITORY_NAME/$BINTRAY_PACKAGE_NAME/versions/$BINTRAY_PACKAGE_VERSION
+	--data '{"username": "'${SONAR_USER}'", "password": "'${SONAR_USER_PASSWORD}'", "close": "1"}' \
+	https://api.bintray.com/maven_central_sync/${CI_DEPLOY_USERNAME}/${BINTRAY_REPOSITORY_NAME}/${BINTRAY_PACKAGE_NAME}/versions/${BINTRAY_PACKAGE_VERSION}
 
 
