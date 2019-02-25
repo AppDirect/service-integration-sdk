d1f782945f44fb8579aae6b74d551ba464cd91ee
Diego <marquesd@gmail.com>
2019-02-25 12:56:32 -0300

    PI-14967: added lombok annotations

--- src/main/java/com/appdirect/sdk/vendorrequiredfields/model/Form.java
+++ src/main/java/com/appdirect/sdk/vendorrequiredfields/model/Form.java
@@ -2,11 +2,15 @@ package com.appdirect.sdk.vendorrequiredfields.model;
 
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Setter
-@Getter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Form {
 
 	private List<VendorRequiredField> fields;
--- src/main/java/com/appdirect/sdk/vendorrequiredfields/model/VendorRequiredFieldResponse.java
+++ src/main/java/com/appdirect/sdk/vendorrequiredfields/model/VendorRequiredFieldResponse.java
@@ -3,17 +3,13 @@ package com.appdirect.sdk.vendorrequiredfields.model;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Builder;
+import lombok.Data;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
+@Data
+@Builder
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class VendorRequiredFieldResponse {
