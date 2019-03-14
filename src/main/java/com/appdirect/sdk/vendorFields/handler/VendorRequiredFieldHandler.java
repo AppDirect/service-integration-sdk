2003521b65eb0d1c31a4d27c9e9cba0fd3fc5d85
Roberto Grings <roberto.grings@appdirect.com>
2019-03-14 11:07:18 -0300

    PI-15191: Changes from CR, package structure and other minor fixes

--- src/main/java/com/appdirect/sdk/vendorFields/handler/VendorRequiredFieldHandler.java
+++ src/main/java/com/appdirect/sdk/vendorFields/handler/VendorRequiredFieldHandler.java
@@ -1,8 +1,4 @@
-package com.appdirect.sdk.vendorFields.requiredFields;
-
-import com.appdirect.sdk.vendorFields.model.FlowType;
-import com.appdirect.sdk.vendorFields.model.OperationType;
-import com.appdirect.sdk.vendorFields.requiredFields.model.VendorRequiredFieldsResponse;
+package com.appdirect.sdk.vendorFields.model;
 
 /**
  * This is the interface you need to implement to apply vendor required requiredFields on a product
