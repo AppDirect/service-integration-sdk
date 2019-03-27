1e42e246fdb7adf7c147812e1ceb350f05af77e0
Roberto Grings <roberto.grings@appdirect.com>
2019-03-27 10:47:34 -0300

    PI-15191: Added @Depracated annotation to older validation controller

--- src/main/java/com/appdirect/sdk/vendorFields/model/FlowType.java
+++ src/main/java/com/appdirect/sdk/vendorFields/model/FlowType.java
@@ -3,17 +3,15 @@ package com.appdirect.sdk.vendorFields.model;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum FlowType {
 	END_USER_FLOW("END_USER_FLOW"), RESELLER_FLOW("RESELLER_FLOW");
 
 	private String value;
-
-	FlowType(String value) {
-		this.value = value;
-	}
 
 	public static FlowType fromValue(String value) {
 		return Stream.of(values())
--- src/main/java/com/appdirect/sdk/vendorFields/model/OperationType.java
+++ src/main/java/com/appdirect/sdk/vendorFields/model/OperationType.java
@@ -3,14 +3,15 @@ package com.appdirect.sdk.vendorFields.model;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum OperationType {
 	SUBSCRIPTION_CHANGE("SUBSCRIPTION_CHANGE"), SUBSCRIPTION_ORDER("SUBSCRIPTION_ORDER");
 
 	private String value;
-
-	OperationType(String value) {
-		this.value = value;
-	}
 
 	public static OperationType fromValue(String value) {
 		return Stream.of(values())
