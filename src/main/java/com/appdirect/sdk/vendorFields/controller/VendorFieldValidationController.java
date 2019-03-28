a948d2a3fbe9c9800174de112620ad850d413ab4
Roberto Grings <roberto.grings@appdirect.com>
2019-03-28 10:21:18 -0300

    PI-15191: removed ApplicationIdentifier field

--- src/main/java/com/appdirect/sdk/vendorFields/controller/VendorFieldValidationController.java
+++ src/main/java/com/appdirect/sdk/vendorFields/controller/VendorFieldValidationController.java
@@ -40,12 +40,11 @@ public class VendorFieldValidationController {
 			produces = APPLICATION_JSON_VALUE)
 	public Callable<VendorFieldsValidationResponse> validateFields(@RequestBody VendorFieldsValidationRequest vendorFieldsValidationRequest) {
 		log.info(
-				"Calling validate fields API with editionCode={}, flowType={}, operationType={}, partner={}, applicationIdentifier={}",
+				"Calling validate fields API with editionCode={}, flowType={}, operationType={}, partner={}",
 				vendorFieldsValidationRequest.getEditionCode(),
 				vendorFieldsValidationRequest.getFlowType(),
 				vendorFieldsValidationRequest.getOperationType(),
-				vendorFieldsValidationRequest.getPartner(),
-				vendorFieldsValidationRequest.getApplicationIdentifier()
+				vendorFieldsValidationRequest.getPartner()
 		);
 		return () -> vendorFieldValidationHandler.validateFields(vendorFieldsValidationRequest);
 	}
