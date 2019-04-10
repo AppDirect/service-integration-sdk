5ad2abfc1009349bb551bfcd81f2f753212d0782
Grings Roberto Eric <roberto.grings@appdirect.com>
2019-04-10 16:01:26 -0300

    PR-15192: Change in import order

--- src/main/java/com/appdirect/sdk/vendorFields/controller/VendorFieldValidationController.java
+++ src/main/java/com/appdirect/sdk/vendorFields/controller/VendorFieldValidationController.java
@@ -1,14 +1,13 @@
 package com.appdirect.sdk.vendorFields.controller;
 
-import com.appdirect.sdk.vendorFields.converter.FlowTypeConverter;
-import com.appdirect.sdk.vendorFields.converter.OperationTypeConverter;
-import com.appdirect.sdk.vendorFields.handler.VendorFieldValidationHandler;
-import com.appdirect.sdk.vendorFields.model.FlowType;
-import com.appdirect.sdk.vendorFields.model.OperationType;
-import com.appdirect.sdk.vendorFields.model.VendorFieldsValidationRequest;
-import com.appdirect.sdk.vendorFields.model.VendorFieldsValidationResponse;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import java.util.concurrent.Callable;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -16,10 +15,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.Callable;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import com.appdirect.sdk.vendorFields.converter.FlowTypeConverter;
+import com.appdirect.sdk.vendorFields.converter.OperationTypeConverter;
+import com.appdirect.sdk.vendorFields.handler.VendorFieldValidationHandler;
+import com.appdirect.sdk.vendorFields.model.FlowType;
+import com.appdirect.sdk.vendorFields.model.OperationType;
+import com.appdirect.sdk.vendorFields.model.VendorFieldsValidationRequest;
+import com.appdirect.sdk.vendorFields.model.VendorFieldsValidationResponse;
 
 /**
  * Defines the endpoint for validating the fields with the vendor
