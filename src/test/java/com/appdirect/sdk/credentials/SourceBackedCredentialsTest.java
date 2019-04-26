927ac07b14f85ed3159ab6631da857561172ad56
Nicolas Magurno <nicolas.magurno@appdirect.com>
2019-04-26 17:43:04 -0300

    PI-15482: adding lombok annotation to Credentials, improve test

--- src/test/java/com/appdirect/sdk/credentials/SourceBackedCredentialsTest.java
+++ src/test/java/com/appdirect/sdk/credentials/SourceBackedCredentialsTest.java
@@ -4,14 +4,14 @@ import com.appdirect.sdk.appmarket.Credentials;
 
 import java.util.function.Function;
 
-import org.assertj.core.api.Condition;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SourceBackedCredentialsTest {
 
-
+	private SourceBackedCredentials findCredentials;
 	private Function<String, Credentials> functionTest = (String k) ->  {
 		if (k.equalsIgnoreCase("bad-key")) {
 			return Credentials.invalidCredentials();
@@ -19,32 +19,32 @@ public class SourceBackedCredentialsTest {
 		return new Credentials(k,"s1");
 	};
 
+	@Before
+	public void setUp() {
+		findCredentials = new SourceBackedCredentials(functionTest);
+	}
+
 	@Test
 	public void returnsGoodCredentials_whenKeyIsFound() throws Exception {
 		//Given
-		SourceBackedCredentials findCredentials = new SourceBackedCredentials(functionTest);
+		Credentials expected = Credentials.builder().developerKey("key1").developerSecret("s1").build();
 
 		//When
 		Credentials credentials = findCredentials.getConsumerCredentials("key1");
 
 		//Then
-		assertThat(credentials).is(credentialsOf("key1", "s1"));
+		assertThat(credentials).isEqualTo(expected);
 	}
 
 	@Test
 	public void returnsInvalidCredentials_whenKeyIsNotFound() throws Exception {
 		//Given
-		SourceBackedCredentials findCredentials = new SourceBackedCredentials(functionTest);
-
+		Credentials expected = Credentials.invalidCredentials();
 
 		//When
 		Credentials credentials = findCredentials.getConsumerCredentials("bad-key");
 
 		//Then
-		assertThat(credentials).is(credentialsOf("this key does not exist in the supplier", "this key does not exist in the supplier"));
-	}
-
-	private Condition<Credentials> credentialsOf(String key, String secret) {
-		return new Condition<>(c -> c.developerKey.equals(key) && c.developerSecret.equals(secret), "credentials of %s:%s", key, secret);
+		assertThat(credentials).isEqualTo(expected);
 	}
 }
