7b4065c4818f08029554c3255f6bd59c68777dd0
Nicolas Magurno <nicolas.magurno@appdirect.com>
2019-04-26 17:43:04 -0300

    PI-15482: adding lombok annotation to Credentials, improve test

--- src/main/java/com/appdirect/sdk/appmarket/Credentials.java
+++ src/main/java/com/appdirect/sdk/appmarket/Credentials.java
@@ -13,12 +13,18 @@
 
 package com.appdirect.sdk.appmarket;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Value;
 
 /**
  * Data object representing the (developerKey, developerSecret) pair shared by any connector using the SDK and the
  * AppMarket. This information is used for signing communications between the two systems
  */
+@Builder
+@AllArgsConstructor
+@Data
 @Value
 public class Credentials {
 	public final String developerKey;
