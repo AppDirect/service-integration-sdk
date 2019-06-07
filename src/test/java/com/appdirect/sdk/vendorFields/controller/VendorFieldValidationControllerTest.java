4b93bbe7ec7cdc8625c3343f8c819d57ad4494dd
Gaston Tulipani <gaston.tulipani@appdirect.com>
2019-06-07 18:36:04 -0300

    Added InitBinder for VendorFieldValidationController and added UT for it

--- src/test/java/com/appdirect/sdk/vendorFields/controller/VendorFieldValidationControllerTest.java
+++ src/test/java/com/appdirect/sdk/vendorFields/controller/VendorFieldValidationControllerTest.java
@@ -1,6 +1,10 @@
 package com.appdirect.sdk.vendorFields.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
@@ -13,7 +17,11 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.bind.WebDataBinder;
 
+import com.appdirect.sdk.vendorFields.converter.FlowTypeConverter;
+import com.appdirect.sdk.vendorFields.converter.LocaleConverter;
+import com.appdirect.sdk.vendorFields.converter.OperationTypeConverter;
 import com.appdirect.sdk.vendorFields.handler.VendorFieldValidationHandler;
 import com.appdirect.sdk.vendorFields.model.FlowType;
 import com.appdirect.sdk.vendorFields.model.OperationType;
@@ -26,6 +34,8 @@ import com.google.inject.internal.Maps;
 public class VendorFieldValidationControllerTest {
 	@Mock
 	private VendorFieldValidationHandler mockVendorFieldValidationHandler;
+	@Mock
+	private WebDataBinder webdataBinder;
 
 	private VendorFieldValidationController vendorFieldValidationController;
 
@@ -60,6 +70,15 @@ public class VendorFieldValidationControllerTest {
 
 		//Then
 		assertThat(controllerResponse).isEqualTo(response);
+	}
+
+	@Test
+	public void testInitBinder() {
+		vendorFieldValidationController.initBinder(webdataBinder);
+
+		verify(webdataBinder, times(1)).registerCustomEditor(eq(FlowType.class), any(FlowTypeConverter.class));
+		verify(webdataBinder, times(1)).registerCustomEditor(eq(OperationType.class), any(OperationTypeConverter.class));
+		verify(webdataBinder, times(1)).registerCustomEditor(eq(List.class), any(LocaleConverter.class));
 	}
 }
 
