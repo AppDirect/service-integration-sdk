19ed6908d8b59109ee0c97ba4d601c25a1014de6
Paul Michard <paul.michard@appdirect.com>
2019-08-01 11:12:35 -0300

    Changed UsageItem attributes types

--- src/main/java/com/appdirect/sdk/meteredusage/model/UsageItem.java
+++ src/main/java/com/appdirect/sdk/meteredusage/model/UsageItem.java
@@ -1,23 +1,29 @@
 package com.appdirect.sdk.meteredusage.model;
 
+import java.math.BigDecimal;
+import java.util.Currency;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import com.appdirect.sdk.appmarket.events.PricingUnit;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class UsageItem {
-	private String pricingUnit;
+	private PricingUnit pricingUnit;
 	private String customUnit;
-	private String quantity;
-	private String unitPrice;
+	private BigDecimal quantity;
+	private BigDecimal unitPrice;
 	private String description;
-	private String currency;
+	private Currency currency;
 	private List<AttributeItem> attributesList;
 }
 
