7e60f57d5e8ba3484f4e24236535b3d1b0f3d61f
Diego <marquesd@gmail.com>
2019-09-30 12:44:21 -0300

    PI-16557 PI-16556: added event date and event id to usage item

--- src/main/java/com/appdirect/sdk/meteredusage/model/UsageItem.java
+++ src/main/java/com/appdirect/sdk/meteredusage/model/UsageItem.java
@@ -1,6 +1,7 @@
 package com.appdirect.sdk.meteredusage.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Currency;
 import java.util.Map;
 
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import com.appdirect.sdk.appmarket.events.PricingUnit;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Builder
 @Data
@@ -25,5 +27,8 @@ public class UsageItem {
 	private String description;
 	private Currency currency;
 	private Map<String, String> attributes;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss")
+	private LocalDateTime eventDate;
+	private String eventId;
 }
 
