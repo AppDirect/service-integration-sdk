cfee1f5151401b2aac85514550d7f6961321a9fb
natalia.ponce <natalia.ponce@appdirect.com>
2019-11-28 16:53:40 -0300

    PI-16960 Added encryption service

---
+++ src/main/java/com/appdirect/sdk/security/encryption/AESEncryptionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.appdirect.sdk.security.encryption;
+
+public class AESEncryptionServiceImpl implements EncryptionService {
+	@Override
+	public String encrypt(String value) throws EncryptionException {
+		return null;
+	}
+
+	@Override
+	public String decrypt(String value) throws EncryptionException {
+		return null;
+	}
+}
---
+++ src/main/java/com/appdirect/sdk/security/encryption/EncryptionException.java
@@ -0,0 +1,10 @@
+package com.appdirect.sdk.security.encryption;
+
+public class EncryptionException extends RuntimeException {
+	public EncryptionException() {
+		super();
+	}
+	public EncryptionException(Throwable cause) {
+		super(cause);
+	}
+}
