fc8095acb235bea7bbab543ce43b2c025a3d10c2
Diego <marquesd@gmail.com>
2019-11-28 16:09:39 -0300

    PI-16966: Added secret to report usage

--- src/main/java/com/appdirect/sdk/meteredusage/service/MeteredUsageApiClientService.java
+++ src/main/java/com/appdirect/sdk/meteredusage/service/MeteredUsageApiClientService.java
@@ -57,7 +57,7 @@ public interface MeteredUsageApiClientService {
 	 * This should be used instead of {@link AppmarketBillingClient} as this replaces the old API version.
 	 *
 	 * @param baseUrl           from the request
-	 * @param key               to sign the request
+	 * @param secretKey               to sign the request
 	 * @param idempotentKey     to make unique calls
 	 * @param meteredUsageItems list of usages to be reported
 	 * @param billable          specifies if the usage to be reported is billable or estimated
@@ -65,5 +65,63 @@ public interface MeteredUsageApiClientService {
 	 * <p>
 	 * throws an {@link MeteredUsageApiException} to the client with an error code and a status:
 	 */
-	APIResult reportUsage(String baseUrl, String key, String idempotentKey, List<MeteredUsageItem> meteredUsageItems, boolean billable);
+	APIResult reportUsage(String baseUrl, String secretKey, String idempotentKey, List<MeteredUsageItem> meteredUsageItems, boolean billable);
+
+	/**
+	 * Calls the Metered Usage API Endpoint to bill usages.
+	 * This should be used instead of {@link AppmarketBillingClient} as this replaces the old API version.
+	 *
+	 * @param baseUrl          from the request
+	 * @param secretKey        to sign the request
+	 * @param idempotentKey    to make unique calls
+	 * @param meteredUsageItem usage instance to be reported
+	 * @param billable         specifies if the usage to be reported is billable or estimated
+	 * @return an {@link APIResult} instance representing the marketplace response
+	 * <p>
+	 * throws an {@link MeteredUsageApiException} to the client with an error code and a status:
+	 */
+	APIResult reportUsage(String baseUrl, String secretKey, String idempotentKey, MeteredUsageItem meteredUsageItem, boolean billable, String secret);
+
+	/**
+	 * Calls the Metered Usage API Endpoint to bill usages.
+	 * This should be used instead of {@link AppmarketBillingClient} as this replaces the old API version.
+	 *
+	 * @param baseUrl          from the request
+	 * @param secretKey        to sign the request
+	 * @param meteredUsageItem usage instance to be reported
+	 * @param billable          specifies if the usage to be reported is billable or estimated
+	 * @return an {@link APIResult} instance representing the marketplace response
+	 * <p>
+	 * throws an {@link MeteredUsageApiException} to the client with an error code and a status:
+	 */
+	APIResult reportUsage(String baseUrl, String secretKey, MeteredUsageItem meteredUsageItem, boolean billable, String secret);
+
+	/**
+	 * Calls the Metered Usage API Endpoint to bill usages.
+	 * This should be used instead of {@link AppmarketBillingClient} as this replaces the old API version.
+	 *
+	 * @param baseUrl           from the request
+	 * @param secretKey         to sign the request
+	 * @param meteredUsageItems list of usages to be reported
+	 * @param billable          specifies if the usage to be reported is billable or estimated
+	 * @return an {@link APIResult} instance representing the marketplace response
+	 * <p>
+	 * throws an {@link MeteredUsageApiException} to the client with an error code and a status:
+	 */
+	APIResult reportUsage(String baseUrl, String secretKey, List<MeteredUsageItem> meteredUsageItems, boolean billable, String secret);
+
+	/**
+	 * Calls the Metered Usage API Endpoint to bill usages.
+	 * This should be used instead of {@link AppmarketBillingClient} as this replaces the old API version.
+	 *
+	 * @param baseUrl           from the request
+	 * @param secretKey               to sign the request
+	 * @param idempotentKey     to make unique calls
+	 * @param meteredUsageItems list of usages to be reported
+	 * @param billable          specifies if the usage to be reported is billable or estimated
+	 * @return an {@link APIResult} instance representing the marketplace response
+	 * <p>
+	 * throws an {@link MeteredUsageApiException} to the client with an error code and a status:
+	 */
+	APIResult reportUsage(String baseUrl, String secretKey, String idempotentKey, List<MeteredUsageItem> meteredUsageItems, boolean billable, String secret);
 }
