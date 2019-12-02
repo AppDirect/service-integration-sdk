895b65f2fe0494f8032afdfe8e799092a06c4c54
natalia.ponce <natalia.ponce@appdirect.com>
2019-12-02 11:11:32 -0300

    PI-16960 Added serializer-deserializer to configuration

--- src/main/java/com/appdirect/sdk/security/encryption/configuration/EncryptionConfiguration.java
+++ src/main/java/com/appdirect/sdk/security/encryption/configuration/EncryptionConfiguration.java
@@ -1,15 +1,25 @@
 package com.appdirect.sdk.security.encryption.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.appdirect.sdk.security.encryption.service.AESEncryptionServiceImpl;
+import com.appdirect.sdk.security.encryption.EncryptDeserialize;
+import com.appdirect.sdk.security.encryption.EncryptSerialize;
 import com.appdirect.sdk.security.encryption.service.EncryptionService;
 
 @Configuration
 public class EncryptionConfiguration {
+	@Autowired
+	private EncryptionService encryptionService;
+
 	@Bean
-	public EncryptionService encryptionService() {
-		return new AESEncryptionServiceImpl();
+	public EncryptDeserialize encryptDeserialize() {
+		return new EncryptDeserialize(encryptionService);
+	}
+	
+	@Bean
+	public EncryptSerialize encryptSerialize() {
+		return new EncryptSerialize(encryptionService);
 	}
 }
