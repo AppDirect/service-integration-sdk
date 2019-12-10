ae2a65d405bf00bb9dd978eb1ddc911561159dd0
Paul Michard <paul.michard@appdirect.com>
2019-12-10 15:05:39 -0300

    PI-16962: Added EncryptionService as part of the EncryptionConfiguration class

--- src/main/java/com/appdirect/sdk/security/encryption/configuration/EncryptionConfiguration.java
+++ src/main/java/com/appdirect/sdk/security/encryption/configuration/EncryptionConfiguration.java
@@ -1,25 +1,34 @@
 package com.appdirect.sdk.security.encryption.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.appdirect.sdk.security.encryption.EncryptDeserialize;
 import com.appdirect.sdk.security.encryption.EncryptSerialize;
+import com.appdirect.sdk.security.encryption.service.AESEncryptionServiceImpl;
 import com.appdirect.sdk.security.encryption.service.EncryptionService;
 
 @Configuration
 public class EncryptionConfiguration {
-	@Autowired
-	private EncryptionService encryptionService;
+
+	@Value("${encryption.key}")
+	private String key;
+	@Value("${encryption.initVector}")
+	private String initVector;
 
 	@Bean
-	public EncryptDeserialize encryptDeserialize() {
+	public EncryptDeserialize encryptDeserialize(EncryptionService encryptionService) {
 		return new EncryptDeserialize(encryptionService);
 	}
-	
+
 	@Bean
-	public EncryptSerialize encryptSerialize() {
+	public EncryptSerialize encryptSerialize(EncryptionService encryptionService) {
 		return new EncryptSerialize(encryptionService);
+	}
+
+	@Bean
+	public EncryptionService encryptionService() {
+		return new AESEncryptionServiceImpl(key, initVector);
 	}
 }
