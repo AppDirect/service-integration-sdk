3acad66913c0cefb3833e6e253be406b5a623160
Carlos Rodriguez <carlos.rodriguez@appdirect.com>
2019-12-30 15:45:58 -0300

    PI-17138 - get the spring context in the constructor with no arg to instantiate the bean, added because is required by the microsoft connector service when consume the API from inbound connector

--- src/main/java/com/appdirect/sdk/security/encryption/EncryptDeserialize.java
+++ src/main/java/com/appdirect/sdk/security/encryption/EncryptDeserialize.java
@@ -1,22 +1,31 @@
 package com.appdirect.sdk.security.encryption;
 
-import java.io.IOException;
 
-import lombok.RequiredArgsConstructor;
-
+import com.appdirect.sdk.security.encryption.configuration.EncryptionConfiguration;
 import com.appdirect.sdk.security.encryption.service.EncryptionService;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@RequiredArgsConstructor
+import java.io.IOException;
+
+@AllArgsConstructor
 public class EncryptDeserialize extends JsonDeserializer<String> {
+
 
 	private final EncryptionService encryptionService;
 
 	@Override
 	public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
 		return encryptionService.decrypt(jsonParser.getValueAsString());
+	}
+
+	public EncryptDeserialize(){
+		ApplicationContext context = new AnnotationConfigApplicationContext(EncryptionConfiguration.class);
+		encryptionService = context.getBean(EncryptionService.class);
 	}
 }
