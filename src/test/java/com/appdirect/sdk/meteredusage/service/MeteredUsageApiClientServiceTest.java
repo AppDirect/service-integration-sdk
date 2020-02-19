c10a562110031c36f281ea3a0f2a060aa915256d
AButelman <andres.butelman@appdirect.com>
2020-02-19 17:02:29 -0300

    PI-17602: Added tests to check if populating the subscriptionId fiel in MeteredUsageItem gets it serialized into Json and leaving it null does not.

--- src/test/java/com/appdirect/sdk/meteredusage/service/MeteredUsageApiClientServiceTest.java
+++ src/test/java/com/appdirect/sdk/meteredusage/service/MeteredUsageApiClientServiceTest.java
@@ -7,6 +7,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
@@ -23,12 +26,14 @@ import com.appdirect.sdk.meteredusage.MeteredUsageApi;
 import com.appdirect.sdk.meteredusage.RetrofitCallStub;
 import com.appdirect.sdk.meteredusage.config.OAuth1RetrofitWrapper;
 import com.appdirect.sdk.meteredusage.model.MeteredUsageItem;
+import com.appdirect.sdk.meteredusage.model.MeteredUsageRequest;
 import com.appdirect.sdk.meteredusage.model.MeteredUsageResponse;
 import com.appdirect.sdk.meteredusage.mother.MeteredUsageItemMother;
 import com.appdirect.sdk.utils.ConstantUtils;
 import com.google.inject.internal.Lists;
 import okhttp3.MediaType;
 import okhttp3.ResponseBody;
+import okio.Buffer;
 import retrofit2.Call;
 import retrofit2.Response;
 
@@ -148,6 +153,51 @@ public class MeteredUsageApiClientServiceTest {
 		APIResult result = meteredUsageApiClientService.reportUsage(ConstantUtils.BASE_URL, ConstantUtils.IDEMPOTENCY_KEY, items, ConstantUtils.BILLABLE, ConstantUtils.CONSUMER_KEY, null, ConstantUtils.EMPTY_SOURCE_TYPE);
 
 		assertThat(result.isSuccess()).isFalse();
+	}
+
+	@Test
+	public void testSendSubscriptionIdInMeteredUsageItem_getsSerialized() {
+
+		Call<MeteredUsageResponse> call = buildCall(MeteredUsageItemMother.withSubscriptionId().build());
+		String body = getCallBodyAsAString(call);
+
+		assertThat(body).isNotNull();
+		assertThat(body).contains(ConstantUtils.SUBSCRIPTION_ID);
+	}
+
+	@Test
+	public void testDONTSendSubscriptionIdInMeteredItem_doesntGetSerialized() {
+
+		Call<MeteredUsageResponse> call = buildCall(MeteredUsageItemMother.basic().build());
+		String body = getCallBodyAsAString(call);
+
+		assertThat(body).isNotNull();
+		assertThat(body).doesNotContain(ConstantUtils.SUBSCRIPTION_ID);
+	}
+
+	private Call<MeteredUsageResponse> buildCall(MeteredUsageItem meteredUsageItem) {
+		MeteredUsageApi meteredUsageApi = meteredUsageApiClientService.createMeteredUsageApi(ConstantUtils.BASE_URL, ConstantUtils.CONSUMER_KEY, ConstantUtils.CONSUMER_SECRET);
+		MeteredUsageRequest meteredUsageRequest = MeteredUsageRequest.builder()
+			.idempotencyKey(ConstantUtils.IDEMPOTENCY_KEY)
+			.billable(ConstantUtils.BILLABLE)
+			.usages(Collections.singletonList(meteredUsageItem))
+			.sourceType(ConstantUtils.SOURCE_TYPE)
+			.build();
+		return meteredUsageApi.meteredUsageCall(meteredUsageRequest);
+	}
+
+	private String getCallBodyAsAString(Call<MeteredUsageResponse> call) {
+		String body = null;
+		try {
+			Buffer buffer = new Buffer();
+			ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			call.request().body().writeTo(buffer);
+			buffer.copyTo(stream);
+			body = new String(stream.toByteArray());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return body;
 	}
 
 	private Response<MeteredUsageResponse> buildResponse(MeteredUsageResponse meteredUsageResponse) {
