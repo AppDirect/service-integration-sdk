cfe0390f7346ebda69318dfe1c82cc45fc3f7f7c
Ajay Aujla <ajay.work@hotmail.com>
2020-06-03 17:54:53 -0400

    PI-18622: Add Partner Code to Required Fields

--- src/main/java/com/appdirect/sdk/vendorFields/handler/VendorRequiredFieldHandlerV2.java
+++ src/main/java/com/appdirect/sdk/vendorFields/handler/VendorRequiredFieldHandlerV2.java
@@ -13,14 +13,15 @@ import com.appdirect.sdk.vendorFields.model.VendorRequiredFieldsResponseV2;
 @FunctionalInterface
 public interface VendorRequiredFieldHandlerV2 {
     // https://appdirect.jira.com/wiki/spaces/PI/pages/982647318/TD+Required+field+Service#3.2.1.1.3-Parameters
-    VendorRequiredFieldsResponseV2 getRequiredFields(String applicationId,
-                                                     String editionId,
-                                                     FlowType flowType,
-                                                     OperationType operationType,
-                                                     String userId,
-                                                     String companyId,
-                                                     String salesAgentUserId,
-                                                     String salesAgentCompanyId,
-                                                     List<Locale> locales
+    VendorRequiredFieldsResponseV2 getRequiredFields(final String applicationId,
+                                                     final String editionId,
+                                                     final FlowType flowType,
+                                                     final OperationType operationType,
+                                                     final String userId,
+                                                     final String companyId,
+                                                     final String salesAgentUserId,
+                                                     final String salesAgentCompanyId,
+                                                     final List<Locale> locales,
+                                                     final String partnerCode
     );
 }
