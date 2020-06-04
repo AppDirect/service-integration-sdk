0768d0c0ddd12f0890c83fbdc2b8f796ba34232c
Ajay Aujla <ajay.work@hotmail.com>
2020-06-04 10:08:55 -0400

    PI-18622: Add Tests, Reformat

--- src/test/java/com/appdirect/sdk/vendorFields/controller/VendorFieldValidationControllerTest.java
+++ src/test/java/com/appdirect/sdk/vendorFields/controller/VendorFieldValidationControllerTest.java
@@ -23,62 +23,99 @@ import com.appdirect.sdk.vendorFields.converter.FlowTypeConverter;
 import com.appdirect.sdk.vendorFields.converter.LocaleConverter;
 import com.appdirect.sdk.vendorFields.converter.OperationTypeConverter;
 import com.appdirect.sdk.vendorFields.handler.VendorFieldValidationHandler;
+import com.appdirect.sdk.vendorFields.handler.VendorFieldValidationHandlerV2;
 import com.appdirect.sdk.vendorFields.model.FlowType;
 import com.appdirect.sdk.vendorFields.model.OperationType;
 import com.appdirect.sdk.vendorFields.model.VendorFieldValidation;
 import com.appdirect.sdk.vendorFields.model.VendorFieldsValidationRequest;
+import com.appdirect.sdk.vendorFields.model.VendorFieldsValidationRequestV2;
 import com.appdirect.sdk.vendorFields.model.VendorFieldsValidationResponse;
+import com.appdirect.sdk.vendorFields.model.VendorFieldsValidationResponseV2;
 import com.google.inject.internal.Maps;
 
 @RunWith(MockitoJUnitRunner.class)
 public class VendorFieldValidationControllerTest {
-	@Mock
-	private VendorFieldValidationHandler mockVendorFieldValidationHandler;
-	@Mock
-	private WebDataBinder webdataBinder;
 
-	private VendorFieldValidationController vendorFieldValidationController;
+    @Mock
+    private VendorFieldValidationHandler mockVendorFieldValidationHandler;
 
-	@Before
-	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
-		vendorFieldValidationController = new VendorFieldValidationController(mockVendorFieldValidationHandler);
-	}
+    @Mock
+    private VendorFieldValidationHandlerV2 mockVendorFieldValidationHandlerV2;
 
-	@Test
-	public void testValidateFields_whenCalled_thenControllerForwardsItsArgumentsToTheUnderlyingHandler() throws Exception {
-		//Given
-		List<Locale> locales = Collections.singletonList(Locale.US);
-		VendorFieldsValidationResponse response = VendorFieldsValidationResponse.builder()
-				.validations(Collections.singletonList(VendorFieldValidation.builder()
-						.fieldName("EMAIL")
-						.errorMessage("must contain @")
-						.build()))
-				.build();
-		VendorFieldsValidationRequest vendorFieldsValidationRequest = VendorFieldsValidationRequest.builder()
-				.fieldValues(Maps.newHashMap())
-				.flowType(FlowType.RESELLER_FLOW)
-				.operationType(OperationType.SUBSCRIPTION_CHANGE)
-				.editionCode("SKU")
-				.partner("APPDIRECT")
-				.build();
-		when(mockVendorFieldValidationHandler.validateFields(vendorFieldsValidationRequest))
-				.thenReturn(response);
-		//When
-		VendorFieldsValidationResponse controllerResponse =
-				vendorFieldValidationController.validateFields(vendorFieldsValidationRequest, locales).call();
+    @Mock
+    private WebDataBinder webdataBinder;
 
-		//Then
-		assertThat(controllerResponse).isEqualTo(response);
-	}
+    private VendorFieldValidationController vendorFieldValidationController;
 
-	@Test
-	public void testInitBinder() {
-		vendorFieldValidationController.initBinder(webdataBinder);
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        vendorFieldValidationController = new VendorFieldValidationController(mockVendorFieldValidationHandler, mockVendorFieldValidationHandlerV2);
+    }
 
-		verify(webdataBinder, times(1)).registerCustomEditor(eq(FlowType.class), any(FlowTypeConverter.class));
-		verify(webdataBinder, times(1)).registerCustomEditor(eq(OperationType.class), any(OperationTypeConverter.class));
-		verify(webdataBinder, times(1)).registerCustomEditor(eq(List.class), any(LocaleConverter.class));
-	}
+    @Test
+    public void testValidateFields_whenCalled_thenControllerForwardsItsArgumentsToTheUnderlyingHandler() throws Exception {
+        //Given
+        List<Locale> locales = Collections.singletonList(Locale.US);
+        VendorFieldsValidationResponse response = VendorFieldsValidationResponse.builder()
+                .validations(Collections.singletonList(VendorFieldValidation.builder()
+                        .fieldName("EMAIL")
+                        .errorMessage("must contain @")
+                        .build()))
+                .build();
+        VendorFieldsValidationRequest vendorFieldsValidationRequest = VendorFieldsValidationRequest.builder()
+                .fieldValues(Maps.newHashMap())
+                .flowType(FlowType.RESELLER_FLOW)
+                .operationType(OperationType.SUBSCRIPTION_CHANGE)
+                .editionCode("SKU")
+                .partner("APPDIRECT")
+                .build();
+        when(mockVendorFieldValidationHandler.validateFields(vendorFieldsValidationRequest))
+                .thenReturn(response);
+        //When
+        VendorFieldsValidationResponse controllerResponse =
+                vendorFieldValidationController.validateFields(vendorFieldsValidationRequest, locales).call();
+
+        //Then
+        assertThat(controllerResponse).isEqualTo(response);
+    }
+
+    @Test
+    public void testValidateFieldsV2_whenCalled_thenControllerForwardsItsArgumentsToTheUnderlyingHandler() throws Exception {
+        //Given
+        List<Locale> locales = Collections.singletonList(Locale.US);
+        String partnerCode = "AD-Tenant";
+        VendorFieldsValidationResponseV2 response = VendorFieldsValidationResponseV2.builder()
+                .validations(Collections.singletonList(VendorFieldValidation.builder()
+                        .fieldName("EMAIL")
+                        .errorMessage("must contain @")
+                        .build()))
+                .build();
+        VendorFieldsValidationRequestV2 vendorFieldsValidationRequest = VendorFieldsValidationRequestV2.builder()
+                .fieldValues(Maps.newHashMap())
+                .flowType(FlowType.RESELLER_FLOW)
+                .operationType(OperationType.SUBSCRIPTION_CHANGE)
+                .editionCode("SKU")
+                .partner("APPDIRECT")
+                .partnerCode(partnerCode)
+                .build();
+        when(mockVendorFieldValidationHandlerV2.validateFields(vendorFieldsValidationRequest))
+                .thenReturn(response);
+        //When
+        VendorFieldsValidationResponseV2 controllerResponse =
+                vendorFieldValidationController.validateFields(vendorFieldsValidationRequest, locales, partnerCode).call();
+
+        //Then
+        assertThat(controllerResponse).isEqualTo(response);
+    }
+
+    @Test
+    public void testInitBinder() {
+        vendorFieldValidationController.initBinder(webdataBinder);
+
+        verify(webdataBinder, times(1)).registerCustomEditor(eq(FlowType.class), any(FlowTypeConverter.class));
+        verify(webdataBinder, times(1)).registerCustomEditor(eq(OperationType.class), any(OperationTypeConverter.class));
+        verify(webdataBinder, times(1)).registerCustomEditor(eq(List.class), any(LocaleConverter.class));
+    }
 }
 
--- src/test/java/com/appdirect/sdk/vendorFields/controller/VendorRequiredFieldsControllerTest.java
+++ src/test/java/com/appdirect/sdk/vendorFields/controller/VendorRequiredFieldsControllerTest.java
@@ -23,70 +23,126 @@ import com.appdirect.sdk.vendorFields.converter.FlowTypeConverter;
 import com.appdirect.sdk.vendorFields.converter.LocaleConverter;
 import com.appdirect.sdk.vendorFields.converter.OperationTypeConverter;
 import com.appdirect.sdk.vendorFields.handler.VendorRequiredFieldHandler;
+import com.appdirect.sdk.vendorFields.handler.VendorRequiredFieldHandlerV2;
 import com.appdirect.sdk.vendorFields.model.FieldType;
 import com.appdirect.sdk.vendorFields.model.FlowType;
 import com.appdirect.sdk.vendorFields.model.Form;
 import com.appdirect.sdk.vendorFields.model.OperationType;
 import com.appdirect.sdk.vendorFields.model.VendorRequiredField;
 import com.appdirect.sdk.vendorFields.model.VendorRequiredFieldsResponse;
+import com.appdirect.sdk.vendorFields.model.VendorRequiredFieldsResponseV2;
 
 @RunWith(MockitoJUnitRunner.class)
 public class VendorRequiredFieldsControllerTest {
-	@Mock
-	private VendorRequiredFieldHandler mockVendorRequiredFieldHandler;
-	@Mock
-	private WebDataBinder webdataBinder;
 
-	private VendorRequiredFieldController vendorRequiredFieldController;
+    @Mock
+    private VendorRequiredFieldHandler mockVendorRequiredFieldHandler;
 
-	@Before
-	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
-		vendorRequiredFieldController = new VendorRequiredFieldController(mockVendorRequiredFieldHandler);
-	}
+    @Mock
+    private VendorRequiredFieldHandlerV2 mockVendorRequiredFieldHandlerV2;
 
-	@Test
-	public void testValidateFields_whenCalled_thenControllerForwardsItsArgumentsToTheUnderlyingHandler() throws Exception {
-		//Given
-		List<Locale> locales = Collections.singletonList(Locale.US);
-		Form form = Form.builder()
-				.fields(Collections.singletonList(VendorRequiredField.builder()
-						.fieldType(FieldType.COUNTRY)
-						.inputCode("ABCDE")
-						.inputTitle("COUNTRY")
-						.order(1)
-						.prePopulatedValue("GERMANY")
-						.required(true)
-						.subTitle("REGION")
-						.toolTip("place your country")
-						.build()))
-				.order(4)
-				.build();
-		VendorRequiredFieldsResponse response = new VendorRequiredFieldsResponse(Collections.singletonList(form));
-		when(mockVendorRequiredFieldHandler.getRequiredFields(
-				"SKU",
-				FlowType.RESELLER_FLOW,
-				OperationType.SUBSCRIPTION_CHANGE,
-				locales)).thenReturn(response);
+    @Mock
+    private WebDataBinder webdataBinder;
 
-		//When
-		VendorRequiredFieldsResponse controllerResponse = vendorRequiredFieldController
-				.getRequiredFields(
-						"SKU",
-						FlowType.RESELLER_FLOW,
-						OperationType.SUBSCRIPTION_CHANGE,
-						locales).call();
+    private VendorRequiredFieldController vendorRequiredFieldController;
 
-		//Then
-		assertThat(controllerResponse).isEqualTo(response);
-	}
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        vendorRequiredFieldController = new VendorRequiredFieldController(mockVendorRequiredFieldHandler, mockVendorRequiredFieldHandlerV2);
+    }
 
-	@Test
-	public void testInitBinder() {
-		vendorRequiredFieldController.initBinder(webdataBinder);
+    @Test
+    public void testGetRequiredFields_whenCalled_thenControllerForwardsItsArgumentsToTheUnderlyingHandler() throws Exception {
+        //Given
+        List<Locale> locales = Collections.singletonList(Locale.US);
+        Form form = Form.builder()
+                .fields(Collections.singletonList(VendorRequiredField.builder()
+                        .fieldType(FieldType.COUNTRY)
+                        .inputCode("ABCDE")
+                        .inputTitle("COUNTRY")
+                        .order(1)
+                        .prePopulatedValue("GERMANY")
+                        .required(true)
+                        .subTitle("REGION")
+                        .toolTip("place your country")
+                        .build()))
+                .order(4)
+                .build();
+        VendorRequiredFieldsResponse response = new VendorRequiredFieldsResponse(Collections.singletonList(form));
+        when(mockVendorRequiredFieldHandler.getRequiredFields(
+                "SKU",
+                FlowType.RESELLER_FLOW,
+                OperationType.SUBSCRIPTION_CHANGE,
+                locales)).thenReturn(response);
 
-		verify(webdataBinder, times(1)).registerCustomEditor(eq(FlowType.class), any(FlowTypeConverter.class));
-		verify(webdataBinder, times(1)).registerCustomEditor(eq(OperationType.class), any(OperationTypeConverter.class));
-		verify(webdataBinder, times(1)).registerCustomEditor(eq(List.class), any(LocaleConverter.class));
-	}
+        //When
+        VendorRequiredFieldsResponse controllerResponse = vendorRequiredFieldController
+                .getRequiredFields(
+                        "SKU",
+                        FlowType.RESELLER_FLOW,
+                        OperationType.SUBSCRIPTION_CHANGE,
+                        locales).call();
+
+        //Then
+        assertThat(controllerResponse).isEqualTo(response);
+    }
+
+    @Test
+    public void testGetRequiredFieldsV2_whenCalled_thenControllerForwardsItsArgumentsToTheUnderlyingHandler() throws Exception {
+        //Given
+        List<Locale> locales = Collections.singletonList(Locale.US);
+        String partnerCode = "AD-Tenant";
+        Form form = Form.builder()
+                .fields(Collections.singletonList(VendorRequiredField.builder()
+                        .fieldType(FieldType.COUNTRY)
+                        .inputCode("ABCDE")
+                        .inputTitle("COUNTRY")
+                        .order(1)
+                        .prePopulatedValue("GERMANY")
+                        .required(true)
+                        .subTitle("REGION")
+                        .toolTip("place your country")
+                        .build()))
+                .order(4)
+                .build();
+        VendorRequiredFieldsResponseV2 response = new VendorRequiredFieldsResponseV2("isvIdentifier", Collections.singletonList(form));
+        when(mockVendorRequiredFieldHandlerV2.getRequiredFields(
+                "SKU",
+                "editionId",
+                FlowType.RESELLER_FLOW,
+                OperationType.SUBSCRIPTION_CHANGE,
+                "userId",
+                "companyId",
+                "salesAgentUserId",
+                "salesAgentCompanyId",
+                locales,
+                partnerCode)).thenReturn(response);
+
+        //When
+        VendorRequiredFieldsResponseV2 controllerResponse = vendorRequiredFieldController
+                .getRequiredFields(
+                        "SKU",
+                        "editionId",
+                        FlowType.RESELLER_FLOW,
+                        OperationType.SUBSCRIPTION_CHANGE,
+                        "userId",
+                        "companyId",
+                        "salesAgentUserId",
+                        "salesAgentCompanyId",
+                        locales,
+                        partnerCode).call();
+
+        //Then
+        assertThat(controllerResponse).isEqualTo(response);
+    }
+
+    @Test
+    public void testInitBinder() {
+        vendorRequiredFieldController.initBinder(webdataBinder);
+
+        verify(webdataBinder, times(1)).registerCustomEditor(eq(FlowType.class), any(FlowTypeConverter.class));
+        verify(webdataBinder, times(1)).registerCustomEditor(eq(OperationType.class), any(OperationTypeConverter.class));
+        verify(webdataBinder, times(1)).registerCustomEditor(eq(List.class), any(LocaleConverter.class));
+    }
 }
