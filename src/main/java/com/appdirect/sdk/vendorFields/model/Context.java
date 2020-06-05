8cfdaee3839e06259ac4bbda2758b5b09bcbc2c6
Ajay Aujla <ajay.work@hotmail.com>
2020-06-05 11:36:53 -0400

    PI-18622: Response V2

---
+++ src/main/java/com/appdirect/sdk/vendorFields/model/Context.java
@@ -0,0 +1,5 @@
+package com.appdirect.sdk.vendorFields.model;
+
+public enum Context {
+    ITEM_LEVEL
+}
---
+++ src/main/java/com/appdirect/sdk/vendorFields/model/VendorRequiredFieldValidationsV2.java
@@ -0,0 +1,20 @@
+package com.appdirect.sdk.vendorFields.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+class VendorRequiredFieldValidationsV2 {
+    private boolean required;
+    private boolean readonly;
+    private String minLength;
+    private String maxLength;
+    private String expression;
+}
