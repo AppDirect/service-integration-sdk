f4c3c96ad58d450302b8ea8f1db307c673c4c631
Aditya Raj <aditya.raj@appdirect.com>
2020-06-17 16:10:57 +0530

    Added logic to store response code as well in APIResult

--- src/main/java/com/appdirect/sdk/meteredusage/exception/ServiceException.java
+++ src/main/java/com/appdirect/sdk/meteredusage/exception/ServiceException.java
@@ -2,16 +2,14 @@ package com.appdirect.sdk.meteredusage.exception;
 
 import lombok.Getter;
 
-import com.appdirect.sdk.appmarket.events.ErrorCode;
-
 @Getter
 public final class ServiceException extends RuntimeException {
 
 	private static final long serialVersionUID = -7023031586871286653L;
 
-	private ErrorCode errorCode;
+	private int errorCode;
 
-	public ServiceException(String message, ErrorCode errorCode) {
+	public ServiceException(String message, int errorCode) {
 		super(message);
 		this.errorCode = errorCode;
 	}
