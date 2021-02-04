2b61c8586d3b528dcfb40611d13c025bd888e0dc
Raushan Amar <raushan.amar@appdirect.com>
2021-02-04 10:43:31 +0530

    Added v2 migration endpoints

--- src/main/java/com/appdirect/sdk/appmarket/migration/AppmarketMigrationController.java
+++ src/main/java/com/appdirect/sdk/appmarket/migration/AppmarketMigrationController.java
@@ -75,4 +75,41 @@ public class AppmarketMigrationController {
 	public Callable<APIResult> migrate(@RequestBody Subscription subscription) {
 		return () -> migrationService.migrate(subscription);
 	}
+
+	/**
+	 * Validate a customer account record that need to be migrated. It is meant to receive the
+	 * input of a migration and indicate if it can be migrated.
+	 *
+	 * @param isvCustomerAccountData A single migration record that is to be validated
+	 * @return an {@link APIResult#success(String)} in case the validation is successful and a {@link APIResult#failure(ErrorCode, String)}
+	 * otherwise.
+	 */
+	@RequestMapping(method = POST, value = "/api/v2/migration/validateCustomerAccount", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+	public Callable<APIResult> validateISVCustomerAccountV2(@RequestBody Map<String, String> isvCustomerAccountData) {
+		return () -> migrationService.validateCustomerAccount(isvCustomerAccountData);
+	}
+
+	/**
+	 * Validate a subscription record that need to be migrated. It is meant to receive the
+	 * input of a migration and indicate if it can be migrated.
+	 *
+	 * @param isvSubscriptionData the subscription record to be migrated
+	 * @return an {@link APIResult#success(String)} in case the validation is successful and a {@link APIResult#failure(ErrorCode, String)}
+	 * otherwise.
+	 */
+	@RequestMapping(method = POST, value = "/api/v2/migration/validateSubscription", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+	public Callable<APIResult> validateISVSubscriptionV2(@RequestBody Map<String, String> isvSubscriptionData) {
+		return () -> migrationService.validateSubscription(isvSubscriptionData);
+	}
+
+	/**
+	 * Migrate a subscription which is already validated by {@link AppmarketMigrationController#validateISVSubscription(Map)}
+	 *
+	 * @param subscription the subscription record to be migrated
+	 * @return an {@link APIResult#success(String)} in case the migration is successful and a {@link APIResult#failure(ErrorCode, String)}
+	 */
+	@RequestMapping(method = POST, value = "/api/v2/migration/subscription", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+	public Callable<APIResult> migrateV2(@RequestBody Subscription subscription) {
+		return () -> migrationService.migrate(subscription);
+	}
 }
