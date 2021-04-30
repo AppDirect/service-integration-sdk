4c89944b63e97ca068be5deebd25c6b01bb3e9f2
saiemmani <76968219+saiemmani@users.noreply.github.com>
2021-04-30 10:37:40 -0400

    Revert "bintray shutdown: try to switch from Travis to Jenkinsfile"

--- src/script/addkeys.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed '/<\/settings>/{ 
-  r src/script/settings
-  a \</settings>
-  d 
-}' mavenSettings | sed -e "s/GPG_PASSPHRASE/$GPG_PASSPHRASE/" -e "s/OSSRH_USERNAME/$OSSRH_USERNAME/" -e "s/OSSRH_PASSWORD/$OSSRH_PASSWORD/" > settings.xml
