030405f7f47cfe5db3b18232b0a2e108cab13107
saiemmani <sai.appdirect.com>
2021-04-30 21:42:24 -0400

    update profiles

--- src/script/addkeys.sh
+++ src/script/addkeys.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+!/bin/bash
 
-sed '/<\/servers>/{ 
-  r servers
-  a \</servers>
-  d 
-}' mavenSettings | sed '/<\/profiles>/{ 
+sed '/<\/profiles>/{ 
   r profiles
   a \</profiles>
   d 
-}' | sed -e "s/GPG_PASSPHRASE/$GPG_PASSPHRASE/" -e "s/OSSRH_USERNAME/$OSSRH_USERNAME/" -e "s/OSSRH_PASSWORD/$OSSRH_PASSWORD/" | > settings.xml
+}' mavenSettings |sed '/<\/servers>/{ 
+  r servers
+  a \</servers>
+  d 
+}' |  sed -e "s/GPG_PASSPHRASE/$GPG_PASSPHRASE/" -e "s/OSSRH_USERNAME/$OSSRH_USERNAME/" -e "s/OSSRH_PASSWORD/$OSSRH_PASSWORD/" > settings.xml
