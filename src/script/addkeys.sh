778d23aed51b822a6a438244c3227f4e43ab6525
Nicolas Zin <nicolas.zin@appdirect.com>
2021-05-03 12:07:40 -0400

    fix src/script/addkeys.sh

--- src/script/addkeys.sh
+++ src/script/addkeys.sh
@@ -1,11 +1,11 @@
-!/bin/bash
+#!/bin/bash
 
 sed '/<\/profiles>/{ 
-  r profiles
+  r src/script/profiles
   a \</profiles>
   d 
 }' mavenSettings |sed '/<\/servers>/{ 
-  r servers
+  r src/script/servers
   a \</servers>
   d 
 }' |  sed -e "s/GPG_PASSPHRASE/$GPG_PASSPHRASE/" -e "s/OSSRH_USERNAME/$OSSRH_USERNAME/" -e "s/OSSRH_PASSWORD/$OSSRH_PASSWORD/" > settings.xml
